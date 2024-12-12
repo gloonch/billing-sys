4f05ebeefa19aa96bd433acbd11bb1631011a5da
gloonch <mahdihadian@hotmail.com>
2024-12-12 20:17:12 +0330

    005: logger and config

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     int
+	DBUser     string
+	DBPassword string
+	DBName     string
+	SSLMode    string
+	HTTPPort   int
+}
+
+func LoadConfig() (*Config, error) {
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	httpPort, _ := strconv.Atoi(os.Getenv("HTTP_PORT"))
+
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     port,
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		SSLMode:    os.Getenv("DB_SSLMODE"),
+		HTTPPort:   httpPort,
+	}, nil
+}
